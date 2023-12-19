abf162a51f93a326709b0b4a71478af86c096f38
Мирзазаде Эмиль Алибей <Mirza_bs61@itstep.edu.az>
2023-12-19 20:23:39 +0400

    Fourth point of HW19-12

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const input=document.querySelector('input');
 
 input.addEventListener("input",(e)=>{
-    const re=/^[0-2][0-4]:[0-5][0-9]$/;
-    console.log(re.test(e.target.value));
+    const re=/\d/;
+    e.target.value=e.target.value.replace(re, 'D');
 });
