ab03c33b6eb5ff4a133cdfa0af0c44542cc847f2
Мирзазаде Эмиль Алибей <Mirza_bs61@itstep.edu.az>
2023-12-19 21:14:12 +0400

    Sixth point of HW19-12

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const input=document.querySelector('input');
-
-input.addEventListener("input",(e)=>{
-    const re=/^[0-2][0-4]:[0-5][0-9]$/;
-    console.log(re.test(e.target.value));
-});
+const text='hello        World      Hi  Heaven!';
+const re=/\s+/;
+const words=text.split(re);
+console.log(words);
+console.log(words.length);
